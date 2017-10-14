a45c9156be014dff9edf3f54432556bdea55f975
Gabriel Fraga <gabrielgoog@gmail.com>
2017-10-14 01:49:59 -0300

    Tela de cadastro de animais

---
+++ VetMaxPitagoras/Foundation/animalForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace VetMaxPitagoras.Foundation
+{
+    partial class animalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtRaca = new System.Windows.Forms.TextBox();
+            this.txtTipo = new System.Windows.Forms.TextBox();
+            this.txtTelefoneDoDono = new System.Windows.Forms.TextBox();
+            this.txtNomeDoDono = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(390, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(392, 228);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Raça:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(397, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tipo:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(332, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Telefone do Dono:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(346, 306);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nome do Dono:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(434, 203);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(174, 20);
+            this.txtNome.TabIndex = 5;
+            // 
+            // txtRaca
+            // 
+            this.txtRaca.Location = new System.Drawing.Point(434, 227);
+            this.txtRaca.Name = "txtRaca";
+            this.txtRaca.Size = new System.Drawing.Size(174, 20);
+            this.txtRaca.TabIndex = 6;
+            // 
+            // txtTipo
+            // 
+            this.txtTipo.Location = new System.Drawing.Point(434, 253);
+            this.txtTipo.Name = "txtTipo";
+            this.txtTipo.Size = new System.Drawing.Size(174, 20);
+            this.txtTipo.TabIndex = 7;
+            // 
+            // txtTelefoneDoDono
+            // 
+            this.txtTelefoneDoDono.Location = new System.Drawing.Point(434, 279);
+            this.txtTelefoneDoDono.Name = "txtTelefoneDoDono";
+            this.txtTelefoneDoDono.Size = new System.Drawing.Size(174, 20);
+            this.txtTelefoneDoDono.TabIndex = 8;
+            // 
+            // txtNomeDoDono
+            // 
+            this.txtNomeDoDono.Location = new System.Drawing.Point(434, 306);
+            this.txtNomeDoDono.Name = "txtNomeDoDono";
+            this.txtNomeDoDono.Size = new System.Drawing.Size(174, 20);
+            this.txtNomeDoDono.TabIndex = 9;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(533, 332);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 10;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            // 
+            // animalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 522);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.txtNomeDoDono);
+            this.Controls.Add(this.txtTelefoneDoDono);
+            this.Controls.Add(this.txtTipo);
+            this.Controls.Add(this.txtRaca);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "animalForm";
+            this.Text = "animalForm";
+            this.Load += new System.EventHandler(this.animalForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtRaca;
+        private System.Windows.Forms.TextBox txtTipo;
+        private System.Windows.Forms.TextBox txtTelefoneDoDono;
+        private System.Windows.Forms.TextBox txtNomeDoDono;
+        private System.Windows.Forms.Button btnCadastrar;
+    }
+}
