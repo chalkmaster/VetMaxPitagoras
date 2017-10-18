0da221af32e4626feab44d3d9e9ed10a93c7163a
Charles Fortes <chalkmaster@gmail.com>
2017-10-18 15:19:13 -0200

    Correção de problemas de merge

--- VetMaxPitagoras.Infrastructure.InMemoryRepository/Security/AnimalRepository.cs
+++ VetMaxPitagoras.Infrastructure.InMemoryRepository/Security/AnimalRepository.cs
@@ -4,26 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VetMaxPitagoras.Infrastructure.Security.Entities;
-<<<<<<< HEAD
 using VetMaxPitagoras.Core.Repository;
-=======
->>>>>>> original
 using VetMaxPitagoras.Infrastructure.Security.Repository;
 
 namespace VetMaxPitagoras.Infrastructure.InMemoryRepository.Security
 {
     public class AnimalRepository : Repository<Animal>, IAnimalRepository
     {
-<<<<<<< HEAD
-
         private static List<Animal> _db;
          
-        public Animal FindByUserName(string nome)
-        {
-            return GetMemoryDatabase().FirstOrDefault(animal => animal.Nome.ToLower() == nome.ToLower());
-=======
-        private static List<Animal> _db;
-
         public List<Animal> FindByName(string nome)
         {
             return GetMemoryDatabase().FindAll(animal => animal.Nome.ToLower().Equals(nome.ToLower()));
@@ -32,7 +21,6 @@ namespace VetMaxPitagoras.Infrastructure.InMemoryRepository.Security
         public List<Animal> FindByProprietarioName(string nome)
         {
             return GetMemoryDatabase().FindAll(animal => animal.Nome.ToLower().Equals(nome.ToLower()));
->>>>>>> original
         }
 
         protected override List<Animal> GetMemoryDatabase()
@@ -41,7 +29,3 @@ namespace VetMaxPitagoras.Infrastructure.InMemoryRepository.Security
         }
     }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> original
