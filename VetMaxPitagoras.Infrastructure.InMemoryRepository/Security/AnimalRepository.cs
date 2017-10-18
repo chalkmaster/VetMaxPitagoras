ffcd866b2f4d0216fc3fc5af45ffaf4424f858e1
DanielaIzabel <31396184+DanielaIzabel@users.noreply.github.com>
2017-10-17 22:06:28 -0200

    Criar cadastro e pesquisa de animal - primeira parte
    Cadastro de animais, parte de inserção de animais e exibição após inserir um animal.

---
+++ VetMaxPitagoras.Infrastructure.InMemoryRepository/Security/AnimalRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VetMaxPitagoras.Infrastructure.Security.Entities;
+using VetMaxPitagoras.Core.Repository;
+using VetMaxPitagoras.Infrastructure.Security.Repository;
+
+namespace VetMaxPitagoras.Infrastructure.InMemoryRepository.Security
+{
+    public class AnimalRepository : Repository<Animal>, IAnimalRepository
+    {
+
+        private static List<Animal> _db;
+         
+        public Animal FindByUserName(string nome)
+        {
+            return GetMemoryDatabase().FirstOrDefault(animal => animal.Nome.ToLower() == nome.ToLower());
+        }
+
+        protected override List<Animal> GetMemoryDatabase()
+        {
+            return _db ?? (_db = new List<Animal>());
+        }
+    }
+}
+
--- VetMaxPitagoras/Foundation/animalForm.cs
+++ VetMaxPitagoras/Foundation/animalForm.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VetMaxPitagoras.Infrastructure.InMemoryRepository.Security;
+using VetMaxPitagoras.Infrastructure.Security.Entities;
+using VetMaxPitagoras.Infrastructure.Security.Services;
 
 namespace VetMaxPitagoras.Foundation
 {
@@ -21,6 +24,29 @@ namespace VetMaxPitagoras.Foundation
         private void animalForm_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            Animal animal = new Animal();
+            animal.Nome = this.txtNome.Text;
+            animal.NomeDono = this.txtNomeDoDono.Text;
+            animal.Raça = this.txtRaca.Text;
+            animal.TelefoneDono = this.txtTelefoneDoDono.Text;
+            animal.TelefoneTipo = this.txtTipo.Text;
+
+            var animalRepository = new AnimalRepository();
+            animalRepository.Insert(animal);
+
+            var animais = animalRepository.FindAll();
+
+            foreach(Animal animalDataGrid in animais){
+                string[] dados = { animalDataGrid.Nome, animalDataGrid.Raça,
+                     animalDataGrid.NomeDono, animalDataGrid.TelefoneDono, animalDataGrid.TelefoneTipo};
+                this.dataCadAnimal.Rows.Add(dados);
+            }
+
+                
         }
     }
 }
