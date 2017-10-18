fc44dede276b83333a93c3bdf73e27eaaddff56c
diego batista <diegobatista_k2@hotmail.com>
2017-10-18 16:01:47 -0200

    Add files via upload
    formulário de cadastro de veterinários

---
+++ VetMaxPitagoras/Foundation/FormCadastroVet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmCadastroVeterinario
+{
+    public partial class FrmCadastroVeterinario : Form
+    {
+        public FrmCadastroVeterinario()
+        {
+            InitializeComponent();
+        }
+
+        private void cadastroVeterinario_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbBairro_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
