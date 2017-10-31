439c408242143d98a1218f07980c921709fcfd2c
lipefili <32708143+lipefili@users.noreply.github.com>
2017-10-31 07:49:49 -0200

    [4SI] centralização cadastro usuário sistema

--- VetMaxPitagoras/Foundation/userForm.Designer.cs
+++ VetMaxPitagoras/Foundation/userForm.Designer.cs
@@ -101,6 +101,7 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.label3);
