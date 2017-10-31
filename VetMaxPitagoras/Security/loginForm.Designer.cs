904298171e9a4d3930eabb8db9760f871901999c
Luciano Romualdo <lucianoromualdo94@gmail.com>
2017-10-30 22:34:34 -0200

    [4SI] ALTERAÇÕES TELA DE LOGIN

--- VetMaxPitagoras/Security/loginForm.Designer.cs
+++ VetMaxPitagoras/Security/loginForm.Designer.cs
@@ -164,9 +164,9 @@
             // 
             // statusStrip1
             // 
+            resources.ApplyResources(this.statusStrip1, "statusStrip1");
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.lbDataHora});
-            resources.ApplyResources(this.statusStrip1, "statusStrip1");
             this.statusStrip1.Name = "statusStrip1";
             // 
             // lbDataHora
