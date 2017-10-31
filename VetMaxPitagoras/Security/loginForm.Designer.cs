4c27fd856e1e746441b7be32e6f18a9714b73e48
Luciano Romualdo <lucianoromualdo94@gmail.com>
2017-10-31 17:05:57 -0200

    [4SI] RETIRADA BOTÃO SAIR TELA DE LOGIN

--- VetMaxPitagoras/Security/loginForm.Designer.cs
+++ VetMaxPitagoras/Security/loginForm.Designer.cs
@@ -32,7 +32,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(loginForm));
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.linkEsqueciSenha = new System.Windows.Forms.LinkLabel();
             this.linkCadastrar = new System.Windows.Forms.LinkLabel();
             this.label2 = new System.Windows.Forms.Label();
@@ -69,14 +68,6 @@
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
             this.label1.Name = "label1";
-            // 
-            // button2
-            // 
-            resources.ApplyResources(this.button2, "button2");
-            this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.Name = "button2";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // linkEsqueciSenha
             // 
@@ -120,7 +111,6 @@
             this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.textBox2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.linkCadastrar);
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.button1);
@@ -206,7 +196,6 @@
         #endregion
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.LinkLabel linkEsqueciSenha;
         private System.Windows.Forms.LinkLabel linkCadastrar;
         private System.Windows.Forms.Label label2;
