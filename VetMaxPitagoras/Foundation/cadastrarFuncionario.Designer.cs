6d169c7258049c405501dda9d55513880399426b
cilash <cilas.henrique@gmail.com>
2017-10-31 22:28:15 -0200

    Reorganização da tela de cadastro de funcionário

--- VetMaxPitagoras/Foundation/cadastrarFuncionario.Designer.cs
+++ VetMaxPitagoras/Foundation/cadastrarFuncionario.Designer.cs
@@ -30,14 +30,14 @@
         {
             this.gridCadastroFuncionario = new System.Windows.Forms.TableLayoutPanel();
             this.grupoCadAnimal = new System.Windows.Forms.GroupBox();
-            this.txtNome = new System.Windows.Forms.TextBox();
-            this.telefoneDono = new System.Windows.Forms.Label();
-            this.lbNomeDono = new System.Windows.Forms.Label();
             this.btnCadastrar = new System.Windows.Forms.Button();
-            this.lbTipo = new System.Windows.Forms.Label();
-            this.lbNome = new System.Windows.Forms.Label();
-            this.lbRaça = new System.Windows.Forms.Label();
             this.txtCpf = new System.Windows.Forms.TextBox();
+            this.lbRaça = new System.Windows.Forms.Label();
+            this.lbNome = new System.Windows.Forms.Label();
+            this.lbTipo = new System.Windows.Forms.Label();
+            this.lbNomeDono = new System.Windows.Forms.Label();
+            this.telefoneDono = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
             this.dtpDataNascimento = new System.Windows.Forms.DateTimePicker();
             this.rdbMasculino = new System.Windows.Forms.RadioButton();
             this.rdbFeminino = new System.Windows.Forms.RadioButton();
@@ -93,31 +93,6 @@
             this.grupoCadAnimal.TabStop = false;
             this.grupoCadAnimal.Text = "Cadastro de Funcionários Administrativos da Clínica";
             // 
-            // txtNome
-            // 
-            this.txtNome.Location = new System.Drawing.Point(129, 37);
-            this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(209, 20);
-            this.txtNome.TabIndex = 5;
-            // 
-            // telefoneDono
-            // 
-            this.telefoneDono.AutoSize = true;
-            this.telefoneDono.Location = new System.Drawing.Point(16, 157);
-            this.telefoneDono.Name = "telefoneDono";
-            this.telefoneDono.Size = new System.Drawing.Size(34, 13);
-            this.telefoneDono.TabIndex = 12;
-            this.telefoneDono.Text = "Sexo:";
-            // 
-            // lbNomeDono
-            // 
-            this.lbNomeDono.AutoSize = true;
-            this.lbNomeDono.Location = new System.Drawing.Point(16, 121);
-            this.lbNomeDono.Name = "lbNomeDono";
-            this.lbNomeDono.Size = new System.Drawing.Size(107, 13);
-            this.lbNomeDono.TabIndex = 4;
-            this.lbNomeDono.Text = "Data de Nascimento:";
-            // 
             // btnCadastrar
             // 
             this.btnCadastrar.Location = new System.Drawing.Point(263, 298);
@@ -127,6 +102,31 @@
             this.btnCadastrar.Text = "Cadastrar";
             this.btnCadastrar.UseVisualStyleBackColor = true;
             this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(129, 77);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(209, 20);
+            this.txtCpf.TabIndex = 6;
+            // 
+            // lbRaça
+            // 
+            this.lbRaça.AutoSize = true;
+            this.lbRaça.Location = new System.Drawing.Point(18, 84);
+            this.lbRaça.Name = "lbRaça";
+            this.lbRaça.Size = new System.Drawing.Size(30, 13);
+            this.lbRaça.TabIndex = 1;
+            this.lbRaça.Text = "CPF:";
+            // 
+            // lbNome
+            // 
+            this.lbNome.AutoSize = true;
+            this.lbNome.Location = new System.Drawing.Point(16, 44);
+            this.lbNome.Name = "lbNome";
+            this.lbNome.Size = new System.Drawing.Size(38, 13);
+            this.lbNome.TabIndex = 0;
+            this.lbNome.Text = "Nome:";
             // 
             // lbTipo
             // 
@@ -138,30 +138,30 @@
             this.lbTipo.Text = "Telefone:";
             this.lbTipo.Click += new System.EventHandler(this.lbTipo_Click);
             // 
-            // lbNome
+            // lbNomeDono
             // 
-            this.lbNome.AutoSize = true;
-            this.lbNome.Location = new System.Drawing.Point(16, 44);
-            this.lbNome.Name = "lbNome";
-            this.lbNome.Size = new System.Drawing.Size(38, 13);
-            this.lbNome.TabIndex = 0;
-            this.lbNome.Text = "Nome:";
+            this.lbNomeDono.AutoSize = true;
+            this.lbNomeDono.Location = new System.Drawing.Point(16, 121);
+            this.lbNomeDono.Name = "lbNomeDono";
+            this.lbNomeDono.Size = new System.Drawing.Size(107, 13);
+            this.lbNomeDono.TabIndex = 4;
+            this.lbNomeDono.Text = "Data de Nascimento:";
             // 
-            // lbRaça
+            // telefoneDono
             // 
-            this.lbRaça.AutoSize = true;
-            this.lbRaça.Location = new System.Drawing.Point(18, 84);
-            this.lbRaça.Name = "lbRaça";
-            this.lbRaça.Size = new System.Drawing.Size(30, 13);
-            this.lbRaça.TabIndex = 1;
-            this.lbRaça.Text = "CPF:";
+            this.telefoneDono.AutoSize = true;
+            this.telefoneDono.Location = new System.Drawing.Point(16, 157);
+            this.telefoneDono.Name = "telefoneDono";
+            this.telefoneDono.Size = new System.Drawing.Size(34, 13);
+            this.telefoneDono.TabIndex = 12;
+            this.telefoneDono.Text = "Sexo:";
             // 
-            // txtCpf
+            // txtNome
             // 
-            this.txtCpf.Location = new System.Drawing.Point(129, 77);
-            this.txtCpf.Name = "txtCpf";
-            this.txtCpf.Size = new System.Drawing.Size(209, 20);
-            this.txtCpf.TabIndex = 6;
+            this.txtNome.Location = new System.Drawing.Point(129, 37);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(209, 20);
+            this.txtNome.TabIndex = 5;
             // 
             // dtpDataNascimento
             // 
@@ -255,21 +255,21 @@
 
         private System.Windows.Forms.TableLayoutPanel gridCadastroFuncionario;
         private System.Windows.Forms.GroupBox grupoCadAnimal;
-        private System.Windows.Forms.TextBox txtNome;
-        private System.Windows.Forms.Label telefoneDono;
-        private System.Windows.Forms.Label lbNomeDono;
         private System.Windows.Forms.Button btnCadastrar;
-        private System.Windows.Forms.Label lbTipo;
-        private System.Windows.Forms.Label lbNome;
-        private System.Windows.Forms.Label lbRaça;
-        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.MaskedTextBox mtbCelular;
         private System.Windows.Forms.MaskedTextBox mtbTelefone;
         private System.Windows.Forms.RadioButton rdbFeminino;
         private System.Windows.Forms.RadioButton rdbMasculino;
         private System.Windows.Forms.DateTimePicker dtpDataNascimento;
-        private System.Windows.Forms.TextBox txtEmail;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label telefoneDono;
+        private System.Windows.Forms.Label lbNomeDono;
+        private System.Windows.Forms.Label lbTipo;
+        private System.Windows.Forms.Label lbNome;
+        private System.Windows.Forms.Label lbRaça;
+        private System.Windows.Forms.TextBox txtCpf;
     }
 }
