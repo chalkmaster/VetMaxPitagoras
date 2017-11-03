b5e43efbc79e93a5367c3ae900f4a746c6c203af
Luciano Romualdo <lucianoromualdo94@gmail.com>
2017-11-03 18:20:19 -0200

    [4SI] [4SI] REMOÇÃO DE CÓDIGO INUTILIZAVEL

--- VetMaxPitagoras/Foundation/EstoqueForm.cs
+++ VetMaxPitagoras/Foundation/EstoqueForm.cs
@@ -17,28 +17,6 @@ namespace VetMaxPitagoras.Foundation
             InitializeComponent();
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
-
-        private void textBox5_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox8_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-        private void textBox9_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox6_TextChanged(object sender, EventArgs e)
-        {
-
-        }
 
         private void tabPage1_Click(object sender, EventArgs e)
         {
