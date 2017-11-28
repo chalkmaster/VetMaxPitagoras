1a9de58d021747518e587c687f0c9c5685793ad8
Luciano Romualdo <lucianoromualdo94@gmail.com>
2017-11-28 16:50:57 -0200

    [4SI] MUDANÇA DE LOCAL DO BOTÃO PESQUISAR

--- VetMaxPitagoras/Foundation/CadastroForm.Designer.cs
+++ VetMaxPitagoras/Foundation/CadastroForm.Designer.cs
@@ -222,10 +222,10 @@ namespace VetMaxPitagoras.Foundation
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.groupBox1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(682, 432);
+            this.tabPage2.Size = new System.Drawing.Size(682, 429);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Cadastrar Cliente";
             this.tabPage2.UseVisualStyleBackColor = true;
